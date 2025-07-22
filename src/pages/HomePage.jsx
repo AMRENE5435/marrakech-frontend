ee22bd476f96b43631fd70075297bba71010163f
AMRENE5435 <nihal.amrene@edu.uiz.ac.ma>
2025-07-22 12:57:56 -0500

    reviews add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -406,7 +406,7 @@ const HomePage = () => {
           </p>
           <div className='flex flex-col sm:flex-row gap-4 justify-center'>
             <Button asChild size='lg' variant='secondary'>
-              <Link to='/add-review'>
+              <Link to='/reviews/add'>
                 <Star className='mr-2 h-5 w-5' />
                 Write a Review
               </Link>
